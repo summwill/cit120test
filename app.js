e202a5e86dc617b9b9778a449bc224037eae8bb4
summwill <89405970+summwill@users.noreply.github.com>
2021-09-05 16:16:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var namePrompt=prompt( message: "What is your name?");
-document.body.querySelector( selectors: ".name").innerHTML=namePrompt;
+var inputBill = Number(prompt( message: "What is your bill?"));
+var taxAmount=inputBill*.07;
+var taxTotalBill=inputBill+taxAmount;
+var tipAmount=taxTotalBill*.05;
+var total=tipAmount+taxTotalBill;
+var element = document.body.querySelector( selectors: ".box");
+
+element.innerHTML=total;
